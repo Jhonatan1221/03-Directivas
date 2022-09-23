0d762571e518e48fe397a952af3d3452258f5371
Jhonatan1221 <108557046+Jhonatan1221@users.noreply.github.com>
2022-09-23 12:45:34 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Directivas en VUE.JS</h1>
+    <hr>
+    <h2>V.text</h2>
+  <span v-text="'hola la directiva v-text'"></span>
+  <br>
+  <samp>hola desde la directiva <v-text></v-text></samp>
+  <br>
+  <span v-text="Titulo"></span>
+  esta es una transmutacion de texto con una <br>
+  {{titulo}}
+  
+
+  </div>
+</template>
+
+<script>
+export default {
+  data:() =>({
+    titulo:"hola desde",
+  }),
+};
+</script>
+<style scoped>
+div{
+  text-align: center;
+  margin: 0px 2rem;
+}
+
+</style>
